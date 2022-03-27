abc62afb2b7da00658df3a53eeb60f3ab42495b8
Karan Barwa <54958197+barwakaranchandra@users.noreply.github.com>
2022-03-27 21:00:11 +0530

    Add files via upload

---
+++ Ass3/login.php
@@ -0,0 +1,141 @@
+<?php
+   ob_start();
+   session_start();
+?>
+
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
+
+<html lang = "en">
+   
+   <head>
+      <title>Login Form</title>
+      <link href = "css/bootstrap.min.css" rel = "stylesheet">
+      
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ADABAB;
+            text-align:center;
+         }
+         
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
+         
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+         
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+         
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+         
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+         
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+         
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+         
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+
+         
+      </style>
+      
+   </head>
+	
+   <body>
+      
+      <h2>Login Form</h2> 
+      <div class = "container form-signin">
+         
+         <?php
+         
+         
+            $msg = '';
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'karanchbarwa3672@gmail.com' && 
+                  $_POST['password'] == '1234') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'karanchbarwa3672@gmail.com';
+                  
+                  echo 'Login Successfully';
+                 
+                  $to = (isset($_GET['username']));
+                  $subject = "Login Successfully";
+                  $txt = "You are loged in our server.";
+                  $headers = "From: karanchbarwa3672@gmail.com" . "\r\n" .
+                  "CC: somebodyelse@example.com";
+                  
+                  mail($to,$subject,$txt,$headers);
+                 
+                  header('Refresh: 2; URL=fupload.php');
+               
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+         ?>
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "karanchbarwa3672@gmail.com" 
+               required autofocus></br><br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "1234" required>
+               <br><br>
+            <button class = "bt" type = "submit" 
+               name = "login">Login</button>
+               
+         </form>
+			<br><br>
+        
+         
+      </div> 
+      
+   </body>
+</html>
